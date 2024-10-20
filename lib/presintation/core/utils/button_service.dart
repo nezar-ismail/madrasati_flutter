33a3eab0320b23f2c82c437a82c85c19fac4d4dd
Nadeem Al-Quraishi <nadeem.alquraiishi@gmail.com>
2024-10-20 17:10:33 +0300

    nadeem

---
+++ lib/presintation/core/utils/button_service.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ButtonService extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  final Color color;
+
+  const ButtonService({
+    super.key,
+    required this.onPressed,
+    required this.text,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        minimumSize: Size(MediaQuery.of(context).size.width * 0.8,
+            MediaQuery.of(context).size.height * 0.1), // Full width button
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: const TextStyle(
+            fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+    );
+  }
+}
---
+++ lib/presintation/features/student/widgets/st_info.dart
@@ -0,0 +1,101 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class ContainerStudentInfo extends StatelessWidget {
+  const ContainerStudentInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.30,
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.lerp(
+              const BorderRadius.only(
+                bottomRight: Radius.circular(20),
+                bottomLeft: Radius.circular(2),
+              ),
+              BorderRadius.vertical(bottom: Radius.circular(20)),
+              6.0),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blue.shade900,
+              const Color.fromARGB(255, 151, 196, 248)
+            ],
+          )),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.grey.shade300,
+            child: const Icon(Icons.person, size: 60, color: Colors.grey),
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+          const Padding(
+            padding: EdgeInsets.only(top: 80.0, bottom: 10.0, left: 1.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'User Name',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'School Name',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  'Class Level',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 80.0, left: 20.0),
+            child: IconButton(
+              icon: const Icon(
+                Icons.notifications_none,
+              ),
+              iconSize: 50,
+              color: Colors.white,
+              hoverColor: Colors.orange,
+              splashColor: Colors.green,
+              onPressed: () {
+                log('Notification Clicked');
+                // Handle notification click
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 300.0, left: 14.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.1,
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.logout),
+                iconSize: 30,
+                color: const Color.fromARGB(255, 218, 157, 66),
+                onPressed: () {
+                  log('Logout Clicked');
+                  // Handle Logout click
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
